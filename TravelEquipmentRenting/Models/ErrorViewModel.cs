4e8a811fe9240783616ef357c5c094ed4cd4f8f2
Emil Dolchinkov <emildolchinkov@gmail.com>
2022-12-05 17:16:21 +0200

    Removed Old Project

--- TravelEquipmentRenting/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TravelEquipmentRenting.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
